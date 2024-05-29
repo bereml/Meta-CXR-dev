4ce4879c7cc7dfa5f1393c8e5f277b62ba14f5dc
bereml <bereml91@gmail.com>
2024-05-29 12:52:06 -0600

    repo cleaning

--- method/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import METHODS
-from .batchbased import BatchBased
-from .episodebased import EpisodeBased
-from .feat import FEAT
-from .fomaml import FOMAML
-from .protonet import ProtoNet
-
-__all__ = ['METHODS']
--- network/__init__.py
+++
@@ -1,9 +0,0 @@
-from .backbones import BACKBONES
-from .factory import create_net, freeze, freeze_and_eval, unfreeze, unfreeze_and_train
-from .models import *
-
-
-__all__ = [
-    'BACKBONES',
-    'create_net', 'freeze', 'freeze_and_eval', 'unfreeze', 'unfreeze_and_train',
-]
