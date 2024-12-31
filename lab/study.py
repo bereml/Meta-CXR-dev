b037db45a7c0d256d4b20d3206a227ee8fb3efcf
bereml <bereml91@gmail.com>
2024-12-31 17:11:27 -0600

    sync last version

--- lab/study.py
+++ lab/study.py
@@ -399,3 +399,69 @@ def study_filter(
             **hparams
         )
         aggregate_exp_df(join(results_dir, exp))
+
+
+def studt_nf(
+        mtrn_episodes=1000,
+        mval_episodes=100,
+        mtst_episodes=10000,
+        net_backbone='mobilenetv3-large-100',
+        max_epochs=150,
+        stop_patience=50,
+        seeds=SEEDS,
+        results_dir=RESULTS_DIR,
+        debug=False):
+    batchbased_train_batches = 0
+    if debug:
+        batchbased_train_batches = 2
+        mtrn_episodes = 2
+        mval_episodes = 2
+        mtst_episodes = 2
+        max_epochs = 2
+        results_dir = 'rdev'
+    exp = 'nf'
+    cfgs = list(product(
+        #   net_weights   method
+        [
+            ['random',    'batchbased'],
+            ['random',    'protonet'],
+        ],
+        seeds,
+    ))
+    for cfg in tqdm(cfgs, desc=f'EXP {exp}', ncols=75):
+        (net_weights, method), seed = cfg
+        run = '_'.join([
+            net_weights,
+            method,
+        ])
+        hparams = {}
+        if 'metachest' == net_weights:
+            net_weights = '_'.join([net_backbone, net_weights, f'seed{seed}'])
+        if method == 'batchbased':
+            hparams['batchbased_train_batches'] = batchbased_train_batches
+            # save checkpoint if not exists
+            checkpoint_name = '_'.join([net_backbone, 'metachest', f'seed{seed}'])
+            if not isfile(join('checkpoints', f'{checkpoint_name}.pth')):
+                hparams['checkpoint_name'] = checkpoint_name
+        elif method == 'episodebased':
+            if net_weights == 'random':
+                mtrn_episodes = 10
+                hparams.update({
+                    'episodebased_mtrn_net_lr': 0.00001,
+                })
+        train_model(
+            results_dir=results_dir,
+            exp=exp,
+            run=run,
+            net_backbone=net_backbone,
+            net_weights=net_weights,
+            method=method,
+            mtrn_episodes=mtrn_episodes,
+            mval_episodes=mval_episodes,
+            mtst_episodes=mtst_episodes,
+            max_epochs=max_epochs,
+            stop_patience=stop_patience,
+            seed=seed,
+            **hparams
+        )
+        aggregate_exp_df(join(results_dir, exp))
