91de6cbb6524785c45e0080a63fd2421b6d6b8fb
bereml <bereml91@gmail.com>
2025-05-03 12:11:53 -0600

    Update study_eval_proto_gfsl exp

--- lab/study.py
+++ lab/study.py
@@ -189,8 +189,8 @@ def study_eval_proto_gfsl(
         # mtst_n_way, mtst_n_unseen
         [
             [3, 1],
-            [3, 2],
-            [3, 3],
+            # [3, 2],
+            # [3, 3],
             # [4, 1],
             # [4, 2],
             # [4, 3],
