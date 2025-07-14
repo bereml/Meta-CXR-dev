d6e6fe642e8b0e2f3b9b21560035c998dc2146f4
bereml <bereml91@gmail.com>
2025-07-14 10:23:48 -0600

    Add paper_shift_ds_bb exp

--- lab/paper.py
+++ lab/paper.py
@@ -893,3 +893,42 @@ def paper_shift_pop_bb(
         )
         aggregate_exp_df(join(results_dir, exp))
 
+
+def paper_shift_ds_bb(
+        seeds=SEEDS,
+        results_dir=RESULTS_DIR,
+        checkpoint_name='mobilenetv3-small-075_i1k+batchbased.pth',
+        debug=False):
+    exp = 'shift_ds_bb'
+    cfgs = list(product(
+        # data_distro
+        [
+            'ds_chestxray14',
+            'ds_chexpert',
+            'ds_mimic',
+            'ds_padchest',
+            'complete',
+        ],
+        seeds,
+    ))
+    for cfg in tqdm(cfgs, desc=f'EXP {exp}', ncols=75):
+        (data_distro, seed) = cfg
+        run = '_'.join([
+            data_distro,
+        ])
+        hparams = {}
+        if debug:
+            hparams.update(DEBUG_HPARAMS_BB)
+            results_dir = 'rdev'
+        eval_model(
+            results_dir=results_dir,
+            exp=exp,
+            run=run,
+            data_distro=data_distro,
+            seed=seed,
+            checkpoint_name=checkpoint_name,
+            **hparams
+        )
+        aggregate_exp_df(join(results_dir, exp))
+
+
