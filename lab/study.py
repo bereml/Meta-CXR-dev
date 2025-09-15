9528804804d7d930e36e6d691441573ad9d9d517
bereml <bereml91@gmail.com>
2025-09-15 15:13:02 -0600

    Add mimic distro exp

--- lab/study.py
+++ lab/study.py
@@ -199,29 +199,30 @@ def study_task_compĺexity_bb_distro(
         results_dir=RESULTS_DIR,
         checkpoint_name='mobilenetv3-small-075_i1k+batchbased.pth',
         debug=False):
-    exp = 'task_compĺexity_distro'
+    exp = 'task_compĺexity_distro_ds_mimic'
     method = 'batchbased'
     cfgs = list(product(
         # mtst_n_way, mtst_n_unseen
         [
             [3, 1],
             [3, 2],
-            [3, 3],
+            #[3, 3],
             [4, 1],
             [4, 2],
             [4, 3],
-            [4, 4],
+            #[4, 4],
             [5, 1],
             [5, 2],
             [5, 3],
             [5, 4],
-            [5, 5],
+            #[5, 5],
         ],
         # mtst_trn_k_shot
         [1, 5, 15, 30],
         # data_distro
         [
             'ds_padchest',
+            'ds_mimic',
         ],
         seeds,
     ))
